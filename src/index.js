7c89816fffa7f1a117dfd86e9edc5cf22d9e7b3d
sunrise <1210353303@qq.com>
2024-06-16 16:09:56 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.ik8s.cc": dockerHub,
+  "quay.ik8s.cc": "https://quay.io",
+  "gcr.ik8s.cc": "https://gcr.io",
+  "k8s-gcr.ik8s.cc": "https://k8s.gcr.io",
+  "k8s.ik8s.cc": "https://registry.k8s.io",
+  "ghcr.ik8s.cc": "https://ghcr.io",
+  "cloudsmith.ik8s.cc": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ik8s.cc": dockerHub,
 };
 
 function routeByHosts(host) {
